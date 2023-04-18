8be678980d870149d4d1fe85912cd1766994bda6
ccprvasu <vasu@ccprllc.com>
2023-04-17 20:23:01 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     //apiUrl: 'http://35.90.149.162:8888/',
     //apiUrl1: 'http://35.90.149.162:8888/ocrnlp/',
   //  apiUrl: 'http://192.168.0.100:8888/',
-    apiUrl:'http://172.31.94.132:8888/',
+    apiUrl:'http://54.152.144.52:8888/',
     production: true
   };
   
