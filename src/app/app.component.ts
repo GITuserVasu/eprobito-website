d3d8486e2c70e3aa07feca7cd736fe4f850ff8cc
ccprvasu <vasu@ccprllc.com>
2023-04-21 09:53:55 -0700

    Added Google Analytics
    Added Google Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+declare const gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eprobito';
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-4FX3V5H4VS', { 'page_path': event.urlAfterRedirects });
+      }      
+    })
+  }
 }
